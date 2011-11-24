3802c0d1524b50bb56c1ecbb71e898746d368467
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-11-24 13:33:14 +0000

    example data

--- filesystem.cpp
+++ filesystem.cpp
@@ -56,15 +56,74 @@ void Filesystem::Dump (int indent /* = 0 */)
 	if ((type == "ext4") || (type == "ext3") || (type == "ext2")) {
 		command = "dump2efs -h " + part->device;
 		command += '0' + part->num;
+#if 0
+		Filesystem volume name:   fedora-16
+		Last mounted on:          /
+		Filesystem UUID:          17d021dd-64dd-4ff6-ab40-97d24407f27e
+		Inode count:              1310720
+		Block count:              5242880
+		Reserved block count:     0
+		Free blocks:              2294731
+		Free inodes:              1076315
+		First block:              0
+		Block size:               4096
+		Fragment size:            4096
+		Journal size:             128M
+
+		2294731*4096
+
+		/dev/sda1      21137846272 8467374080 12670472192  41% /
+
+		fs total size		21474836480
+		df total size		21137846272 (128M journal + 60M inodes + 133M other)
+
+		used			8467374080
+		free			12670472192
+		total			21137846272
+#endif
 	} else if (type == "linux-swap(v1)") {
 		command = "cat /proc/meminfo";
+#if 0
+		SwapCached:        20472 kB
+		SwapTotal:       7164952 kB
+		SwapFree:        6972292 kB
+#endif
 	} else if (type == "ntfs") {
 		command = "ntfsresize -P -i -f -v " + part->device;
 		command += '0' + part->num;
+#if 0
+		Device name        : /dev/sda7
+		Cluster size       : 4096 bytes
+		Current volume size: 193651012096 bytes (193652 MB)
+		Current device size: 193651015680 bytes (193652 MB)
+		Space in use       : 74 MB (0.0%)
+		You might resize at 73482240 bytes or 74 MB (freeing 193578 MB).
+#endif
 	} else if (type == "fat32") {
 		command = "dosfsck -n -v " + part->device;
 		command += '0' + part->num;
+#if 0
+		512 bytes per logical sector
+		16384 bytes per cluster
+			32 reserved sectors
+		First FAT starts at byte 16384 (sector 32)
+			2 FATs, 32 bit entries
+		34504704 bytes per FAT (= 67392 sectors)
+		Root directory start at cluster 2 (arbitrary size)
+		Data area starts at byte 69025792 (sector 134816)
+		8625355 data clusters (141317816320 bytes)
+		63 sectors/track, 255 heads
+		276146176 sectors total
+		/dev/sda6: 1 files, 262145/8625355 clusters
+#endif
 	}
+
+#if 0
+	df -B1 /dev/sda7
+	Filesystem        1B-blocks      Used    Available Use% Mounted on
+	/dev/sda7      193651011584 479731712 193171279872   1% /mnt/vg/junk/robert
+
+#endif
 
 	//result = execute_command (command, output, error);
 
