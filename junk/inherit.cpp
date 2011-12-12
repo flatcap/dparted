0ab56e62e8d4b2ccc598b01c862d92829eaa7e7d
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-12-12 15:57:51 +0000

    inherit

---
+++ junk/inherit.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+class CString : public std::string
+{
+public:
+	CString (const char *str) :
+		std::string (str)
+	{
+	}
+
+	operator long()
+	{
+		return strtol (c_str(), NULL, 10);
+	}
+};
+
+int main()
+{
+	CString s ("1234");
+	long num = s;
+
+	printf ("s is '%s'\n", s.c_str());
+	printf ("s is %lu chars long\n", s.size());
+	printf ("num = %ld\n", num);
+
+	return 0;
+}
