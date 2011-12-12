fc3c21a7fdfdb008535be4dac40da9cabfec60ba
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-12-12 14:49:38 +0000

    metadata

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC	= g++
 
-SRC	= container.cpp disk.cpp filesystem.cpp main.cpp mount.cpp msdos.cpp partition.cpp segment.cpp utils.cpp volume.cpp volumegroup.cpp whole.cpp
-HDR	= container.h disk.h filesystem.h mount.h msdos.h partition.h segment.h utils.h volume.h volumegroup.h whole.h
+SRC	= container.cpp disk.cpp filesystem.cpp main.cpp metadata.cpp mount.cpp msdos.cpp partition.cpp segment.cpp utils.cpp volume.cpp volumegroup.cpp whole.cpp
+HDR	= container.h disk.h filesystem.h metadata.h mount.h msdos.h partition.h segment.h utils.h volume.h volumegroup.h whole.h
 OBJ	= $(SRC:.cpp=.o)
 
 OUT	= main
@@ -29,12 +29,13 @@ force:
 container.o:	container.cpp container.h utils.h
 disk.o:		disk.cpp disk.h container.h utils.h
 filesystem.o:	filesystem.cpp filesystem.h container.h partition.h utils.h
-main.o:		main.cpp container.h disk.h filesystem.h partition.h volumegroup.h volume.h utils.h
+main.o:		main.cpp container.h disk.h filesystem.h mount.h msdos.h partition.h segment.h volumegroup.h whole.h volume.h utils.h
+metadata.o:	metadata.cpp metadata.h container.h utils.h
 mount.o:	mount.cpp mount.h container.h utils.h
 msdos.o:	msdos.cpp msdos.h container.h utils.h
 partition.o:	partition.cpp partition.h container.h utils.h
-segmented.o:	segmented.cpp container.h
+segment.o:	segment.cpp segment.h container.h
 utils.o:	utils.cpp utils.h
-volume.o:	volume.cpp volume.h container.h utils.h
-volumegroup.o:	volumegroup.cpp volumegroup.h container.h utils.h
-
+volume.o:	volume.cpp volume.h whole.h container.h utils.h
+volumegroup.o:	volumegroup.cpp volumegroup.h whole.h container.h utils.h
+whole.o:	whole.cpp whole.h container.h
---
+++ metadata.cpp
@@ -0,0 +1,76 @@
+/* Copyright (c) 2011 Richard Russon (FlatCap)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+#include <stdio.h>
+#include <string>
+#include <sstream>
+
+#include "metadata.h"
+#include "utils.h"
+
+/**
+ * Metadata
+ */
+Metadata::Metadata (void)
+{
+	type = "metadata";
+}
+
+/**
+ * ~Metadata
+ */
+Metadata::~Metadata()
+{
+}
+
+
+/**
+ * dump_dot
+ */
+std::string Metadata::dump_dot (void)
+{
+	std::ostringstream output;
+
+	output << dump_table_header ("Metadata", "white");
+
+	// no specfics for now
+
+	output << Container::dump_dot();
+
+	output << dump_table_footer();
+
+	// now iterate through all the children
+	for (std::vector<Container*>::iterator i = children.begin(); i != children.end(); i++) {
+		output << "\n";
+		output << (*i)->dump_dot();
+		output << "obj_" << this << " -> " << "obj_" << (*i) << ";\n";
+		output << "\n";
+	}
+
+#if 0
+	// now iterate through all the children
+	for (std::vector<Container*>::iterator i = children.begin(); i != children.end(); i++) {
+		output << "\n";
+		output << (*i)->dump_dot();
+		output << "obj_" << this << " -> " << "obj_" << (*i) << ";\n";
+		output << "\n";
+	}
+#endif
+
+	return output.str();
+}
