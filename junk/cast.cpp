951c5256b131619b3dafb69a0ffef97eea6039f5
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-12-12 04:57:11 +0000

    more casts

--- junk/cast.cpp
+++ junk/cast.cpp
@@ -5,24 +5,27 @@
 class A
 {
 public:
-	operator int() { return 5; }
-	operator long() { return 6; }
-	operator long long() { return 7; }
-	operator unsigned int() { return 8; }
+	operator int()		{ return 5; }
+	operator long()		{ return 6; }
+	operator long long()	{ return 7; }
+	operator unsigned int()	{ return 8; }
+	operator const char *()	{ return "hello world"; }
 };
 
 int main (int argc, char *argv[])
 {
-	A a;
-	int b = a; // No cast is required.
-	long c = a;
-	long long d = a;
-	unsigned int e = a;
+	A		 a;
+	int		 b = a; // No casts required
+	long		 c = a;
+	long long	 d = a;
+	unsigned int	 e = a;
+	const	char	*f = a;
 
-	printf ("b = %d\n", b);
-	printf ("c = %ld\n", c);
+	printf ("b = %d\n",   b);
+	printf ("c = %ld\n",  c);
 	printf ("d = %lld\n", d);
-	printf ("e = %d\n", e);
+	printf ("e = %d\n",   e);
+	printf ("f = %s\n",   f);
 
 	return b;
 }
