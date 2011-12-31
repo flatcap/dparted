96222bb5ca6c45550ee7af15b778624a90b4a7a7
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-12-31 07:24:43 +0000

    rename shuffle volume

--- images/shuffle_create.sh
+++ images/shuffle_create.sh
@@ -15,23 +15,23 @@ pvcreate /dev/loop3
 
 vgcreate $VOLUME /dev/loop3
 
-lvcreate --size 52m --name data  $VOLUME
-lvcreate --size 52m --name junk1 $VOLUME
-lvcreate --size 52m --name junk2 $VOLUME
-lvcreate --size 40m --name junk3 $VOLUME
+lvcreate --size 52m --name jigsaw $VOLUME
+lvcreate --size 52m --name junk1  $VOLUME
+lvcreate --size 52m --name junk2  $VOLUME
+lvcreate --size 40m --name junk3  $VOLUME
 
 lvremove -f /dev/$VOLUME/junk2
-lvextend --size +52m /dev/$VOLUME/data
+lvextend --size +52m /dev/$VOLUME/jigsaw
 lvremove -f /dev/$VOLUME/junk1
-lvextend --size +52m /dev/$VOLUME/data
+lvextend --size +52m /dev/$VOLUME/jigsaw
 lvremove -f /dev/$VOLUME/junk3
-lvextend --size +40m /dev/$VOLUME/data
+lvextend --size +40m /dev/$VOLUME/jigsaw
 
-mke2fs -t ext4 -L shuffle_data /dev/$VOLUME/data
+mke2fs -t ext4 -L "$VOLUME jigsaw" /dev/$VOLUME/jigsaw
 
 lvs --all -o lv_name,vg_name,lv_attr,lv_size,lv_uuid,lv_path,lv_kernel_major,lv_kernel_minor,seg_count $VOLUME
 
-populate_ext4 /dev/$VOLUME/data 100M
+populate_ext4 /dev/$VOLUME/jigsaw 100M
 
 rm -f $IMAGE3
 
