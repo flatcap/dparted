65a9a408da6ec70388fd4612b8420920fa4f2016
Richard Russon (flatcap) <richard.russon@gmail.com>
2012-01-08 19:34:19 +0000

    sizeof (off_t)

--- junk/size.c
+++ junk/size.c
@@ -9,6 +9,7 @@ int main (int argc, char *argv[])
 	printf ("sizeof (long)      = %ld\n", sizeof (long));
 	printf ("sizeof (long long) = %ld\n", sizeof (long long));
 	printf ("sizeof (size_t)    = %ld\n", sizeof (size_t));
+	printf ("sizeof (off_t)     = %ld\n", sizeof (off_t));
 
 	return 0;
 }
