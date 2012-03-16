202ebb93368daf776c58e018dd2af5f2b98b519b
Richard Russon (flatcap) <richard.russon@gmail.com>
2012-03-16 21:54:33 +0000

    simplify partition creation

--- images/common.sh
+++ images/common.sh
@@ -23,3 +23,68 @@ function populate()
 }
 
 
+function gpt_init()
+{
+	[ -z "$1" ] && return
+	[ -z "$2" ] || return
+	[ -f "$1" ] || return
+
+	cat <<-EOF | parted $1
+		mklabel gpt
+		quit
+	EOF
+}
+
+function gpt_print()
+{
+	[ -z "$1" ] && return
+	[ -z "$2" ] || return
+	[ -f "$1" ] || return
+
+	cat <<-EOF | parted $1
+		unit s
+		print free
+		quit
+	EOF
+
+}
+
+function gpt_create()
+{
+	[ -z "$7" ] && return
+	[ -z "$8" ] || return
+	[ -b "$1" ] || return
+	[ -f "$2" ] || return
+
+	local LOOP="$1"
+	local FILE="$2"
+	local PLABEL="$3"
+	local START="$4"
+	local SIZE="$5"
+	local FLABEL="$6"
+	local FILL="$7"
+	local END="$((START+SIZE))"
+
+	START=$((START*2048))
+	SIZE=$((SIZE*2048))
+	END=$((END*2048))
+
+	cat <<-EOF | parted $FILE
+		unit s
+		mkpart primary $START $((END-1))
+		name $COUNT $PLABEL
+		quit
+	EOF
+
+	START=$((START*512))
+	SIZE=$((SIZE*512))
+
+	losetup $LOOP $FILE --offset $START --sizelimit $SIZE
+	mke2fs -t ext4 -L $FLABEL $LOOP
+	populate_ext4 $LOOP ${FILL}M
+	losetup -d $LOOP
+
+	: $((COUNT++))
+}
+
+
--- images/vm_create.sh
+++ images/vm_create.sh
@@ -1,60 +1,26 @@
 #!/bin/bash
 
-IMAGE4="disk4"
+FILE4="disk4"
+LOOP4="/dev/loop4"
+COUNT=0
 
 source common.sh
 
-rm -f $IMAGE4
-fallocate --length 1G $IMAGE4
+rm -f $FILE4
+fallocate --length 1G $FILE4
 
-cat <<-EOF | parted $IMAGE4
-	mklabel gpt
+gpt_init $FILE4
 
-	unit s
+#           loop    disk    part  disk off size filesystem fill
+#           device  image   label     MiB  MiB  label       MiB
+gpt_create  $LOOP4  $FILE4  first       1   99  first_fs     20
+gpt_create  $LOOP4  $FILE4  second    100  100  second_fs    60
+gpt_create  $LOOP4  $FILE4  third     200  300  third_fs    180
+gpt_create  $LOOP4  $FILE4  fourth    500  300  fourth_fs    90
+gpt_create  $LOOP4  $FILE4  fifth     800  220  fifth_fs    180
 
-	mkpart primary     2048   204799
-	mkpart primary   204800   409599
-	mkpart primary   409600  1023999
-	mkpart primary  1024000  1638399
-	mkpart primary  1638400  2097118
+gpt_print $FILE4
 
-	name 1 first
-	name 2 second
-	name 3 third
-	name 4 fourth
-	name 5 fifth
-
-	print free
-
-	quit
-EOF
-
-losetup /dev/loop4 $IMAGE4 --offset $((512*2048)) --sizelimit $((512*(204800-2048)))
-mke2fs -t ext4 -L first_fs /dev/loop4
-populate_ext4 /dev/loop4 20M
-losetup -d /dev/loop4
-
-losetup /dev/loop4 $IMAGE4 --offset $((512*204800)) --sizelimit $((512*(409600-204800)))
-mke2fs -t ext4 -L second_fs /dev/loop4
-populate_ext4 /dev/loop4 60M
-losetup -d /dev/loop4
-
-losetup /dev/loop4 $IMAGE4 --offset $((512*409600)) --sizelimit $((512*(1024000-409600)))
-mke2fs -t ext4 -L third_fs /dev/loop4
-populate_ext4 /dev/loop4 180M
-losetup -d /dev/loop4
-
-losetup /dev/loop4 $IMAGE4 --offset $((512*1024000)) --sizelimit $((512*(1638400-1024000)))
-mke2fs -t ext4 -L fourth_fs /dev/loop4
-populate_ext4 /dev/loop4 90M
-losetup -d /dev/loop4
-
-losetup /dev/loop4 $IMAGE4 --offset $((512*1638400)) --sizelimit $((512*(2097119-1638400)))
-mke2fs -t ext4 -L fifth_fs /dev/loop4
-populate_ext4 /dev/loop4 180M
-losetup -d /dev/loop4
-
-losetup /dev/loop4 $IMAGE4
-
-rm -f $IMAGE4
+losetup /dev/loop4 $FILE4
+rm -f $FILE4
 
