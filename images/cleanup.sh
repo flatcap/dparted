7c35f845af6b1f6328e18eff10ed65cdca704fc7
Richard Russon (flatcap) <richard.russon@gmail.com>
2012-03-21 15:25:03 +0000

    full cleanup

--- images/cleanup.sh
+++ images/cleanup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-function cleanup()
-{
-	lvremove -f /dev/mapper/test_* > /dev/null 2>&1
-	local LOOPS="$(losetup -a | cut -d: -f1 | tac)"
-	if [ -n "$LOOPS" ]; then
-		for i in $LOOPS; do
-			sync
-			losetup -d $i
-		done
-	fi
+source common.sh
 
-	rm -f test_*.img
-}
+lvremove -f /dev/mapper/test_* > /dev/null 2>&1
+LOOPS="$(losetup -a | cut -d: -f1)"
+if [ -n "$LOOPS" ]; then
+	for i in $LOOPS; do
+		losetup -d $i
+	done
+fi
 
+rm -f test_*.img
 
