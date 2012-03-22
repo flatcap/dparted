a953b26ec8f9bb2273dd0b1b4f65ab9d988aae15
Richard Russon (flatcap) <richard.russon@gmail.com>
2012-03-22 03:00:05 +0000

    modernise bigvol(raid10) script

--- images/raid10_create.sh
+++ images/raid10_create.sh
@@ -1,22 +1,53 @@
 #!/bin/bash
 
-VOLUME="bigvol"
+IMAGE_SIZE="5G"
 
 source common.sh
 
-for i in $(enum 8 15); do
-	fallocate -l 1G disk$i
-	losetup /dev/loop$i disk$i
-	rm -f disk$i
-done
+function test_bigvol()
+{
+	local IMAGE
+	local LOOP
+	local LOOP_LIST
+	local GROUP=$FUNCNAME
+	local INDEX=${FUNCNAME##*_}
+	local VOLUME="$INDEX"
 
-pvcreate /dev/loop{8,9,10,11,12,13,14,15}
+	echo -n "$FUNCNAME: "
 
-vgcreate $VOLUME /dev/loop{8,9,10,11,12,13,14,15}
+	for i in {1..8}; do
+		IMAGE="$(create_image ${FUNCNAME}$i)"
+		[ -n "$IMAGE" -a -f "$IMAGE" ] || error || return
 
-lvcreate $VOLUME -i 2 -m 3 --name bob --size 320M
+		LOOP="$(create_loop $IMAGE)"
+		[ -n "$LOOP" -a -b "$LOOP" ] || error || return
 
-mke2fs -t ext4 -L "$VOLUME bob" /dev/$VOLUME/bob
+		pvcreate "$LOOP" > /dev/null 2>&1
+		[ $? = 0 ] || error || return
 
-populate_ext4 /dev/$VOLUME/bob 250M
+		LOOP_LIST="$LOOP_LIST $LOOP"
+	done
+
+	vgcreate $GROUP $LOOP_LIST > /dev/null
+	[ $? = 0 ] || error || return
+
+	lvcreate $GROUP -i 2 -m 3 --name bob --size 320M > /dev/null
+	[ $? = 0 ] || error || return
+
+	mke2fs -t ext4 -q -L "$GROUP bob" /dev/mapper/$GROUP-bob
+	[ $? = 0 ] || error || return
+
+	#populate_ext4 /dev/mapper/$VOLUME-bob 250M
+
+	ok "$LOOP_LIST"
+}
+
+if [ $# = 0 ]; then
+	cleanup
+	test_bigvol
+elif [ $# = 1 -a $1 = "-d" ]; then
+	cleanup
+else
+	usage
+fi
 
