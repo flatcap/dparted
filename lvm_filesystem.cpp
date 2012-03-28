5bff4b1f7900ba9180c615523703af1ac450946f
Richard Russon (flatcap) <richard.russon@gmail.com>
2012-03-28 14:04:28 +0100

    add lvm filesystem

---
+++ lvm_filesystem.cpp
@@ -0,0 +1,53 @@
+/* Copyright (c) 2012 Richard Russon (FlatCap)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+#include <sstream>
+
+#include "lvm_filesystem.h"
+
+/**
+ * LVMFilesystem
+ */
+LVMFilesystem::LVMFilesystem (void)
+{
+	declare ("lvm_filesystem");
+}
+
+/**
+ * ~LVMFilesystem
+ */
+LVMFilesystem::~LVMFilesystem()
+{
+}
+
+
+/**
+ * dump_dot
+ */
+std::string LVMFilesystem::dump_dot (void)
+{
+	std::ostringstream output;
+
+	// no specifics for now
+
+	output << Filesystem::dump_dot();
+
+	return output.str();
+}
+
+
