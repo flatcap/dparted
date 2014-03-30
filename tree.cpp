1c263bb591b8dd7a7ba310c674441e650b2b62ff
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-03-31 00:09:41 +0100

    drop old code

--- tree.cpp
+++ tree.cpp
@@ -32,43 +32,18 @@ Tree::Tree()
 
 	Gtk::TreeModel::ColumnRecord cols;
 
-#if 1
 	mod_cols.push_back (ModColPtr (new Gtk::TreeModelColumn<int>));		// 0 id
 	mod_cols.push_back (ModColPtr (new Gtk::TreeModelColumn<std::string>));	// 1 name
-#else
-	Gtk::TreeModelColumn<int>     m_col_id;
-	Gtk::TreeModelColumn<Glib::ustring> m_col_name;
-#endif
 
-#if 1
 	cols.add(*mod_cols[0]);	// id
 	cols.add(*mod_cols[1]); // name
-#else
-	cols.add(m_col_id);
-	cols.add(m_col_name);
-#endif
 
 	m_TreeStore = Gtk::TreeStore::create(cols);
 	m_TreeView.set_model(m_TreeStore);
 
-	Gtk::TreeModel::Row row = *(m_TreeStore->append());		// iterator
-	std::string james = "jim";
-#if 1
-	row->set_value (0, 42);		// id
-	row->set_value (1, james);      // name
-#else
-	row[m_col_id]   = 42;
-	row[m_col_name] = james;
-#endif
-
-#if 1
-	// m_TreeView.append_column("ID",   *mod_cols[0]);
-	// m_TreeView.append_column("Name", *mod_cols[1]);
-
 	Gtk::CellRenderer* pCellRenderer = nullptr;
 	Gtk::TreeView::Column* col = nullptr;
 
-	//Add the TreeView's view columns:
 	pCellRenderer = manage(new Gtk::CellRendererText());
 	col = Gtk::manage (new Gtk::TreeView::Column ("ID", *pCellRenderer));
 	col->set_renderer(*pCellRenderer, *mod_cols[0]);
@@ -78,10 +53,12 @@ Tree::Tree()
 	col = Gtk::manage (new Gtk::TreeView::Column ("Name", *pCellRenderer));
 	col->set_renderer(*pCellRenderer, *mod_cols[1]);
 	m_TreeView.append_column (*col);
-#else
-	m_TreeView.append_column("ID",   m_col_id);
-	m_TreeView.append_column("Name", m_col_name);
-#endif
+
+	Gtk::TreeModel::Row row = *(m_TreeStore->append());		// iterator
+	std::string james = "jim";
+
+	row->set_value (0, 42);		// id
+	row->set_value (1, james);      // name
 
 	show_all_children();
 }
