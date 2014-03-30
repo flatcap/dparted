ec61550f34c90ee8d1b6da06b77b5e71259b393a
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-03-30 15:10:52 +0100

    dynamic placeholders

--- tree.cpp
+++ tree.cpp
@@ -20,6 +20,10 @@ main(int argc, char *argv[])
 	return app->run(window);
 }
 
+
+typedef std::shared_ptr<Gtk::TreeModelColumnBase> ModColPtr;
+std::vector<ModColPtr> mod_cols;
+
 Tree::Tree()
 {
 	set_default_size(400, 200);
@@ -27,21 +31,42 @@ Tree::Tree()
 	add(m_TreeView);
 
 	Gtk::TreeModel::ColumnRecord cols;
-	Gtk::TreeModelColumn<int> m_col_id;
-	Gtk::TreeModelColumn<Glib::ustring> m_col_name;
 
+#if 0
+	mod_cols.push_back (ModColPtr (new Gtk::TreeModelColumn<int>));		// 0 id
+	mod_cols.push_back (ModColPtr (new Gtk::TreeModelColumn<std::string>));	// 1 name
+#else
+	Gtk::TreeModelColumn<int>     m_col_id;
+	Gtk::TreeModelColumn<Glib::ustring> m_col_name;
+#endif
+
+#if 0
+	cols.add(*mod_cols[0]);	// id
+	cols.add(*mod_cols[1]); // name
+#else
 	cols.add(m_col_id);
 	cols.add(m_col_name);
+#endif
 
 	m_refTreeModel = Gtk::TreeStore::create(cols);
 	m_TreeView.set_model(m_refTreeModel);
 
 	Gtk::TreeModel::Row row = *(m_refTreeModel->append());
+#if 0
+	row[*mod_cols[0]] = 1;		// id
+	row[*mod_cols[1]] = "jim";	// name
+#else
 	row[m_col_id]   = 1;
 	row[m_col_name] = "jim";
+#endif
 
+#if 0
+	m_TreeView.append_column("ID",   *mod_cols[0]);
+	m_TreeView.append_column("Name", *mod_cols[1]);
+#else
 	m_TreeView.append_column("ID",   m_col_id);
 	m_TreeView.append_column("Name", m_col_name);
+#endif
 
 	show_all_children();
 }
