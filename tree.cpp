26b617652fd4e77d3c8025caf4b94e62b33625ad
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-03-31 00:12:53 +0100

    extra column

--- tree.cpp
+++ tree.cpp
@@ -34,9 +34,11 @@ Tree::Tree()
 
 	mod_cols.push_back (ModColPtr (new Gtk::TreeModelColumn<int>));		// 0 id
 	mod_cols.push_back (ModColPtr (new Gtk::TreeModelColumn<std::string>));	// 1 name
+	mod_cols.push_back (ModColPtr (new Gtk::TreeModelColumn<std::string>));	// 2 family
 
 	cols.add(*mod_cols[0]);	// id
 	cols.add(*mod_cols[1]); // name
+	cols.add(*mod_cols[2]); // family
 
 	m_TreeStore = Gtk::TreeStore::create(cols);
 	m_TreeView.set_model(m_TreeStore);
@@ -54,11 +56,18 @@ Tree::Tree()
 	col->set_renderer(*pCellRenderer, *mod_cols[1]);
 	m_TreeView.append_column (*col);
 
-	Gtk::TreeModel::Row row = *(m_TreeStore->append());		// iterator
-	std::string james = "jim";
+	pCellRenderer = manage(new Gtk::CellRendererText());
+	col = Gtk::manage (new Gtk::TreeView::Column ("Surname", *pCellRenderer));
+	col->set_renderer(*pCellRenderer, *mod_cols[2]);
+	m_TreeView.append_column (*col);
 
-	row->set_value (0, 42);		// id
-	row->set_value (1, james);      // name
+	Gtk::TreeModel::Row row = *(m_TreeStore->append());		// iterator
+	std::string name    = "James";
+	std::string surname = "Cagney";
+
+	row->set_value (0, 42);
+	row->set_value (1, name);
+	row->set_value (2, surname);
 
 	show_all_children();
 }
