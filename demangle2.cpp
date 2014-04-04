c0dfd60a766abead627d3280293edaf7cfe48d5f
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-04-04 20:07:14 +0100

    c++ tidy

--- demangle2.cpp
+++ demangle2.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <string>
@@ -6,19 +7,19 @@
 #include <cxxabi.h>
 #include <execinfo.h>
 
-#define MAX_FRAMES 100
+std::vector<std::string> my_bt;
 
 std::string
 demangle (const char* symbol)
 {
-	size_t size;
-	int status;
-	char temp[128];
-	char* demangled;
+	std::vector<char> buffer;
+	buffer.resize (256);	// Most prototypes should be shorter than this
+
+	char* demangled = nullptr;
 
 	// C++ stack: demangle2(_Z7mytracev+0x20) [0x4019f1]
-	if (sscanf (symbol, "%*[^(]%*[^_]%127[^)+]", temp) == 1) {
-		if (NULL != (demangled = abi::__cxa_demangle (temp, NULL, &size, &status))) {
+	if (sscanf (symbol, "%*[^(]%*[^_]%255[^)+]", buffer.data()) == 1) {
+		if ((demangled = abi::__cxa_demangle (buffer.data(), NULL, NULL, NULL))) {
 			std::string result (demangled);
 			free (demangled);
 			return result;
@@ -26,37 +27,44 @@ demangle (const char* symbol)
 	}
 
 	// Typeinfo: 3barI5emptyLi17EE
-	if ((demangled = abi::__cxa_demangle (symbol, NULL, &size, &status)) != NULL) {
+	if ((demangled = abi::__cxa_demangle (symbol, NULL, NULL, NULL))) {
 		std::string result (demangled);
 		free (demangled);
 		return result;
 	}
 
 	// C stack: demangle2(main+0x19) [0x401bc8]
-	if (sscanf (symbol, "%*[^(](%127[^)+]", temp) == 1) {
-		if (strncmp (temp, "main", 4) == 0)
+	if (sscanf (symbol, "%*[^(](%255[^)+]", buffer.data()) == 1) {
+		if (strncmp (buffer.data(), "main", 4) == 0) {
 			return "main(int, char**)";
-		else
-			return temp;
+		} else {
+			return buffer.data();
+		}
 	}
 
 	return symbol;
 }
 
-void
-mytrace (void)
+std::vector<std::string>
+get_backtrace (bool reverse = true)
 {
+	const int MAX_FRAMES = 100;
+	std::vector<std::string> bt;
+
 	void* addresses[MAX_FRAMES];
 	int size = backtrace (addresses, MAX_FRAMES);
 	char** symbols = backtrace_symbols (addresses, size);
-	for (int x = 0; x < size; ++x) {
+	for (int x = 1; x < size; ++x) {			// Skip 0 (ourself)
 		std::string sym = demangle (symbols[x]);
-		printf ("%s\n", demangle (symbols[x]).c_str());
-
-		if (sym.substr (0, 4) == "main")
+		bt.push_back (sym);
+		if (sym.substr (0, 4) == "main")		// Just libc after this
 			break;
 	}
 	free (symbols);
+	if (reverse) {
+		std::reverse (std::begin (bt), std::end (bt));
+	}
+	return bt;
 }
 
 std::vector<std::string>
@@ -64,7 +72,7 @@ c (int w, const char *msg)
 {
 	(void) w;
 	(void) msg;
-	mytrace();
+	my_bt = get_backtrace();
 
 	return {};
 }
@@ -90,6 +98,11 @@ int
 main()
 {
 	a (42);
+
+	for (auto i : my_bt) {
+		std::cout << i << std::endl;
+	}
+
 	return 0;
 }
 
