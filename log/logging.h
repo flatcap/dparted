0c1c46553b5df5739a7b0f28bbb54c14d68cf17e
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-04-12 00:38:53 +0100

    drop old ideas

--- log/logging.h
+++
@@ -1,6 +0,0 @@
-log_handler log_handler_syslog  __attribute__((format(printf, 6, 0)));
-log_handler log_handler_fprintf __attribute__((format(printf, 6, 0)));
-log_handler log_handler_null    __attribute__((format(printf, 6, 0)));
-log_handler log_handler_stdout  __attribute__((format(printf, 6, 0)));
-log_handler log_handler_outerr  __attribute__((format(printf, 6, 0)));
-log_handler log_handler_stderr  __attribute__((format(printf, 6, 0)));
