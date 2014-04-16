edec6e5fcca2034d87089a997b22f34a9008598c
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-04-17 00:00:13 +0100

    thread logging

--- log_macro.h
+++ log_macro.h
@@ -32,6 +32,7 @@
 #define log_file             log_redirect
 #define log_ctor             log_redirect
 #define log_dtor             log_redirect
+#define log_thread           log_redirect
 #else
 // Log level: EMERG
 #define log_system_emergency(...) log_redirect(Severity::SystemEmergency,__PRETTY_FUNCTION__,__FILE__,__LINE__,__VA_ARGS__)
@@ -69,6 +70,7 @@
 #define log_file(...)             log_redirect(Severity::File,           __PRETTY_FUNCTION__,__FILE__,__LINE__,__VA_ARGS__)
 #define log_ctor(...)             log_redirect(Severity::Ctor,           __PRETTY_FUNCTION__,__FILE__,__LINE__,__VA_ARGS__)
 #define log_dtor(...)             log_redirect(Severity::Dtor,           __PRETTY_FUNCTION__,__FILE__,__LINE__,__VA_ARGS__)
+#define log_thread(...)           log_redirect(Severity::Thread,         __PRETTY_FUNCTION__,__FILE__,__LINE__,__VA_ARGS__)
 #endif
 
 #endif // _LOG_MACRO_H_
