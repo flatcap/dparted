d5287f0328b4b742306ff9fc34bcc9bef550d3bf
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-04-27 00:27:53 +0100

    rework question dialog

--- question_dialog.cpp
+++ question_dialog.cpp
@@ -1,59 +1,99 @@
+/* Copyright (c) 2014 Richard Russon (FlatCap)
+ *
+ * This file is part of DParted.
+ *
+ * DParted is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DParted is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DParted.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "question_dialog.h"
+#define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
 
 QuestionDialog::QuestionDialog (void) :
-	MessageDialog ("", true, Gtk::MessageType::MESSAGE_OTHER, Gtk::ButtonsType::BUTTONS_NONE, false)
+	MessageDialog ("", true, Gtk::MessageType::MESSAGE_QUESTION, Gtk::ButtonsType::BUTTONS_NONE, false)
 {
-#if 0
-	MESSAGE_INFO
-	MESSAGE_WARNING
-	MESSAGE_QUESTION
-	MESSAGE_ERROR
-	MESSAGE_OTHER
-#endif
-	// set_size_request (400, 400);
-
-	Gtk::Box* ma = get_message_area();
-	ma->pack_start (text1);
-
-	Gtk::Box* ca = get_content_area();
-	ca->pack_start (text2);
-
-	// Gtk::ButtonBox* bb = get_action_area();
-
-	add_button ("b_1", 101);
-	add_button ("b_2", 102);
-	add_button ("b_3", 103);
-
-	signal_response().connect (std::bind (&QuestionDialog::on_dialog_response, this, std::placeholders::_1));
-
-#if 1
-	const char* icon =
-		"Information"		// key
-		//"dialog-password"		// key
-		//"dialog-information"		// light bulb
-		//"dialog-warning"		// yellow triangle
-		//"dialog-error"		// red cross
-		//"dialog-apply"		// green tick
-	;
-	i.set_from_icon_name (icon, Gtk::BuiltinIconSize::ICON_SIZE_DIALOG);
-	set_image(i);
-#endif
-
-	// set_title ("title");
-	set_message ("message message message message message");
-	set_secondary_text ("secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text, secondary text");
-
-	show_all();
-}
-
-void
-QuestionDialog::on_dialog_response (int response_id)
-{
-	log_debug ("Button: %d\n", response_id);
+	signal_response().connect(sigc::mem_fun(this,&QuestionDialog::on_dialog_response));
 }
 
 QuestionDialog::~QuestionDialog()
 {
+}
+
+QuestionDialogPtr
+QuestionDialog::create (void)
+{
+	return QuestionDialogPtr (new QuestionDialog());
+}
+
+void
+QuestionDialog::on_dialog_response (int button_id)
+{
+	log_debug ("Button: %d\n", button_id);
+}
+
+int
+QuestionDialog::run (void)
+{
+	Gtk::Button help ("_Help", true);
+	if (!help_url.empty()) {
+		Gtk::ButtonBox* bb = get_action_area();
+		bb->pack_end (help);
+		bb->set_child_secondary (help);
+		help.signal_clicked().connect (sigc::mem_fun (this, &QuestionDialog::on_help));
+	}
+
+	for (auto& i : buttons) {
+		add_button (i.first, i.second);
+	}
+
+	set_title (title);
+	set_message (primary);
+	set_secondary_text (secondary);
+
+	show_all();
+	show_all_children();
+
+	return Gtk::MessageDialog::run();
+}
+
+void
+QuestionDialog::on_help (void)
+{
+	GError *error = nullptr;
+	gtk_show_uri (nullptr, help_url.c_str(), 0, &error);
+	if (error) {
+		log_debug ("Can't open uri: %s\n", error->message);
+		g_error_free (error);
+	}
+	log_debug ("HELP: %s\n", help_url.c_str());
+}
+
+bool
+QuestionDialog::on_key_press_event (GdkEventKey* event)
+{
+	if (event->keyval == GDK_KEY_Escape) {
+		// Ignore the escape key
+		return true;
+	} else {
+		// Leave everything else alone
+		return MessageDialog::on_key_press_event (event);
+	}
+}
+
+bool
+QuestionDialog::on_event (GdkEvent* event)
+{
+	// Prevent Alt-F4 (delete event)
+	return (event->type == GdkEventType::GDK_DELETE);
 }
 
--- question_dialog.h
+++ question_dialog.h
@@ -1,22 +1,60 @@
+/* Copyright (c) 2014 Richard Russon (FlatCap)
+ *
+ * This file is part of DParted.
+ *
+ * DParted is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DParted is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DParted.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _QUESTION_DIALOG_H_
 #define _QUESTION_DIALOG_H_
 
+#include <memory>
+
 #include <gtkmm/messagedialog.h>
+#include <gtkmm/dialog.h>
+#include <gtkmm/window.h>
 #include <gtkmm/entry.h>
+#include <gtkmm/image.h>
 #include <gtkmm/button.h>
+
+class QuestionDialog;
+
+typedef std::shared_ptr<QuestionDialog> QuestionDialogPtr;
 
 class QuestionDialog : public Gtk::MessageDialog
 {
 public:
-	QuestionDialog (void);
 	virtual ~QuestionDialog();
 
-protected:
-	Gtk::Entry text1;
-	Gtk::Entry text2;
-	Gtk::Image i;
+	static QuestionDialogPtr create (void);
 
-	void on_dialog_response (int response_id);
+	std::string title;
+	std::string primary;
+	std::string secondary;
+	std::string help_url;
+
+	std::vector<std::pair<std::string,int>> buttons;
+
+	int run (void);		// Hide Dialog::run
+
+protected:
+	QuestionDialog (void);
+	void on_dialog_response (int button_id);
+	virtual bool on_key_press_event (GdkEventKey* event);
+	virtual bool on_event (GdkEvent * event);
+
+	void on_help (void);
 };
 
 #endif // _QUESTION_DIALOG_H_
