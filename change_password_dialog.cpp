1ed0054db8f50687863e8f20b85147a9b11494ed
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-04-27 02:56:34 +0100

    rework change password dialog

--- change_password_dialog.cpp
+++ change_password_dialog.cpp
@@ -19,6 +19,20 @@ ChangePasswordDialog::ChangePasswordDialog (void) :
 	ca->pack_start (text2);
 	ca->pack_start (text3);
 
+	sp_box.pack_start (sp_toggle, Gtk::PackOptions::PACK_SHRINK);
+	sp_box.pack_start (sp_label, Gtk::PackOptions::PACK_SHRINK);
+	ca->pack_start (sp_box);
+	sp_toggle.set_active (false);
+	sp_toggle.signal_toggled().connect (sigc::mem_fun(this,&ChangePasswordDialog::on_sp_toggle));
+
+	sp_label.set_use_underline (true);
+	sp_label.set_text_with_mnemonic ("Show _Password");
+	sp_label.set_mnemonic_widget (sp_toggle);
+
+	text1.set_visibility (false);
+	text2.set_visibility (false);
+	text3.set_visibility (false);
+
 	add_button ("b_1", 101);
 	add_button ("b_2", 102);
 	add_button ("b_3", 103);
@@ -42,14 +56,30 @@ ChangePasswordDialogPtr
 ChangePasswordDialog::create (void)
 {
 	ChangePasswordDialogPtr p (new ChangePasswordDialog());
-	p->self = p;
-
 	return p;
 }
 
 int
 ChangePasswordDialog::run (void)
 {
+	Gtk::Button help ("_Help", true);
+	if (!help_url.empty()) {
+		Gtk::ButtonBox* bb = get_action_area();
+		bb->pack_end (help);
+		bb->set_child_secondary (help);
+		help.signal_clicked().connect (sigc::mem_fun (this, &ChangePasswordDialog::on_help));
+	}
+
+	set_title (title);
+	set_message (primary);
+	set_secondary_text (secondary);
+
+	for (auto& i : buttons) {
+		add_button (i.first, i.second);
+	}
+
+	show_all();
+
 	return Gtk::MessageDialog::run();
 }
 
@@ -58,4 +88,25 @@ ChangePasswordDialog::on_dialog_response (int response_id)
 {
 	log_debug ("Button: %d\n", response_id);
 }
+
+void
+ChangePasswordDialog::on_sp_toggle (void)
+{
+	text1.set_visibility (sp_toggle.get_active());
+	text2.set_visibility (sp_toggle.get_active());
+	text3.set_visibility (sp_toggle.get_active());
+}
+
+void
+ChangePasswordDialog::on_help (void)
+{
+	GError *error = nullptr;
+	gtk_show_uri (nullptr, help_url.c_str(), 0, &error);
+	if (error) {
+		log_debug ("Can't open uri: %s\n", error->message);
+		g_error_free (error);
+	}
+	log_debug ("HELP: %s\n", help_url.c_str());
+}
+
 
--- change_password_dialog.h
+++ change_password_dialog.h
@@ -5,6 +5,8 @@
 
 #include <gtkmm/image.h>
 #include <gtkmm/entry.h>
+#include <gtkmm/checkbutton.h>
+#include <gtkmm/label.h>
 
 #include <gtkmm/messagedialog.h>
 
@@ -20,19 +22,28 @@ public:
 	static ChangePasswordDialogPtr create (void);
 
 	std::string title;
+	std::string primary;
+	std::string secondary;
+	std::string help_url;
+
+	std::vector<std::pair<std::string,int>> buttons;
 
 	int run (void);		// Hide Dialog::run
 
 protected:
 	ChangePasswordDialog();
 	void on_dialog_response (int response_id);
+	void on_sp_toggle (void);
+	void on_help (void);
 
 	Gtk::Entry text1;
 	Gtk::Entry text2;
 	Gtk::Entry text3;
 	Gtk::Image image;
 
-	std::weak_ptr<ChangePasswordDialog> self;
+	Gtk::CheckButton sp_toggle;
+	Gtk::Box sp_box;
+	Gtk::Label sp_label;
 };
 
 #endif // _CHANGE_PASSWORD_DIALOG_H_
