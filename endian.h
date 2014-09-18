625a301727d7dcb5b2b48f5eb543d2958aba80e7
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-09-18 21:53:08 +0100

    whitespace

--- endian.h
+++ endian.h
@@ -25,83 +25,83 @@
 
 // Unsigned: From LE to CPU
 
-#define le16_to_cpu(x)		(std::uint16_t)le16toh((std::uint16_t)(x))
-#define le32_to_cpu(x)		(std::uint32_t)le32toh((std::uint32_t)(x))
-#define le64_to_cpu(x)		(std::uint64_t)le64toh((std::uint64_t)(x))
+#define le16_to_cpu(x)		(std::uint16_t) le16toh ((std::uint16_t)(x))
+#define le32_to_cpu(x)		(std::uint32_t) le32toh ((std::uint32_t)(x))
+#define le64_to_cpu(x)		(std::uint64_t) le64toh ((std::uint64_t)(x))
 
-#define le16_to_cpup(x)		(std::uint16_t)le16toh(*(const std::uint16_t*)(x))
-#define le32_to_cpup(x)		(std::uint32_t)le32toh(*(const std::uint32_t*)(x))
-#define le64_to_cpup(x)		(std::uint64_t)le64toh(*(const std::uint64_t*)(x))
+#define le16_to_cpup(x)		(std::uint16_t) le16toh (*(const std::uint16_t*)(x))
+#define le32_to_cpup(x)		(std::uint32_t) le32toh (*(const std::uint32_t*)(x))
+#define le64_to_cpup(x)		(std::uint64_t) le64toh (*(const std::uint64_t*)(x))
 
 // Signed: From LE to CPU
 
-#define sle16_to_cpu(x)		(std::int16_t)le16toh((std::int16_t)(x))
-#define sle32_to_cpu(x)		(std::int32_t)le32toh((std::int32_t)(x))
-#define sle64_to_cpu(x)		(std::int64_t)le64toh((std::int64_t)(x))
+#define sle16_to_cpu(x)		(std::int16_t) le16toh ((std::int16_t)(x))
+#define sle32_to_cpu(x)		(std::int32_t) le32toh ((std::int32_t)(x))
+#define sle64_to_cpu(x)		(std::int64_t) le64toh ((std::int64_t)(x))
 
-#define sle16_to_cpup(x)	(std::int16_t)le16toh(*(const std::int16_t*)(x))
-#define sle32_to_cpup(x)	(std::int32_t)le32toh(*(const std::int32_t*)(x))
-#define sle64_to_cpup(x)	(std::int64_t)le64toh(*(const std::int64_t*)(x))
+#define sle16_to_cpup(x)	(std::int16_t) le16toh (*(const std::int16_t*)(x))
+#define sle32_to_cpup(x)	(std::int32_t) le32toh (*(const std::int32_t*)(x))
+#define sle64_to_cpup(x)	(std::int64_t) le64toh (*(const std::int64_t*)(x))
 
 // Unsigned: From CPU to LE
 
-#define cpu_to_le16(x)		(std::uint16_t)htole16((std::uint16_t)(x))
-#define cpu_to_le32(x)		(std::uint32_t)htole32((std::uint32_t)(x))
-#define cpu_to_le64(x)		(std::uint64_t)htole64((std::uint64_t)(x))
+#define cpu_to_le16(x)		(std::uint16_t) htole16 ((std::uint16_t)(x))
+#define cpu_to_le32(x)		(std::uint32_t) htole32 ((std::uint32_t)(x))
+#define cpu_to_le64(x)		(std::uint64_t) htole64 ((std::uint64_t)(x))
 
-#define cpu_to_le16p(x)		(std::uint16_t)htole16(*(const std::uint16_t*)(x))
-#define cpu_to_le32p(x)		(std::uint32_t)htole32(*(const std::uint32_t*)(x))
-#define cpu_to_le64p(x)		(std::uint64_t)htole64(*(const std::uint64_t*)(x))
+#define cpu_to_le16p(x)		(std::uint16_t) htole16 (*(const std::uint16_t*)(x))
+#define cpu_to_le32p(x)		(std::uint32_t) htole32 (*(const std::uint32_t*)(x))
+#define cpu_to_le64p(x)		(std::uint64_t) htole64 (*(const std::uint64_t*)(x))
 
 // Signed: From CPU to LE
 
-#define cpu_to_sle16(x)		(std::int16_t)htole16((std::int16_t)(x))
-#define cpu_to_sle32(x)		(std::int32_t)htole32((std::int32_t)(x))
-#define cpu_to_sle64(x)		(std::int64_t)htole64((std::int64_t)(x))
+#define cpu_to_sle16(x)		(std::int16_t) htole16 ((std::int16_t)(x))
+#define cpu_to_sle32(x)		(std::int32_t) htole32 ((std::int32_t)(x))
+#define cpu_to_sle64(x)		(std::int64_t) htole64 ((std::int64_t)(x))
 
-#define cpu_to_sle16p(x)	(std::int16_t)htole16(*(const std::int16_t*)(x))
-#define cpu_to_sle32p(x)	(std::int32_t)htole32(*(const std::int32_t*)(x))
-#define cpu_to_sle64p(x)	(std::int64_t)htole64(*(const std::int64_t*)(x))
+#define cpu_to_sle16p(x)	(std::int16_t) htole16 (*(const std::int16_t*)(x))
+#define cpu_to_sle32p(x)	(std::int32_t) htole32 (*(const std::int32_t*)(x))
+#define cpu_to_sle64p(x)	(std::int64_t) htole64 (*(const std::int64_t*)(x))
 
 // Unsigned: From BE to CPU
 
-#define be16_to_cpu(x)		(std::uint16_t)be16toh((std::uint16_t)(x))
-#define be32_to_cpu(x)		(std::uint32_t)be32toh((std::uint32_t)(x))
-#define be64_to_cpu(x)		(std::uint64_t)be64toh((std::uint64_t)(x))
+#define be16_to_cpu(x)		(std::uint16_t) be16toh ((std::uint16_t)(x))
+#define be32_to_cpu(x)		(std::uint32_t) be32toh ((std::uint32_t)(x))
+#define be64_to_cpu(x)		(std::uint64_t) be64toh ((std::uint64_t)(x))
 
-#define be16_to_cpup(x)		(std::uint16_t)be16toh(*(const std::uint16_t*)(x))
-#define be32_to_cpup(x)		(std::uint32_t)be32toh(*(const std::uint32_t*)(x))
-#define be64_to_cpup(x)		(std::uint64_t)be64toh(*(const std::uint64_t*)(x))
+#define be16_to_cpup(x)		(std::uint16_t) be16toh (*(const std::uint16_t*)(x))
+#define be32_to_cpup(x)		(std::uint32_t) be32toh (*(const std::uint32_t*)(x))
+#define be64_to_cpup(x)		(std::uint64_t) be64toh (*(const std::uint64_t*)(x))
 
 // Signed: From BE to CPU
 
-#define sbe16_to_cpu(x)		(std::int16_t)be16toh((std::int16_t)(x))
-#define sbe32_to_cpu(x)		(std::int32_t)be32toh((std::int32_t)(x))
-#define sbe64_to_cpu(x)		(std::int64_t)be64toh((std::int64_t)(x))
+#define sbe16_to_cpu(x)		(std::int16_t) be16toh ((std::int16_t)(x))
+#define sbe32_to_cpu(x)		(std::int32_t) be32toh ((std::int32_t)(x))
+#define sbe64_to_cpu(x)		(std::int64_t) be64toh ((std::int64_t)(x))
 
-#define sbe16_to_cpup(x)	(std::int16_t)be16toh(*(const std::int16_t*)(x))
-#define sbe32_to_cpup(x)	(std::int32_t)be32toh(*(const std::int32_t*)(x))
-#define sbe64_to_cpup(x)	(std::int64_t)be64toh(*(const std::int64_t*)(x))
+#define sbe16_to_cpup(x)	(std::int16_t) be16toh (*(const std::int16_t*)(x))
+#define sbe32_to_cpup(x)	(std::int32_t) be32toh (*(const std::int32_t*)(x))
+#define sbe64_to_cpup(x)	(std::int64_t) be64toh (*(const std::int64_t*)(x))
 
 // Unsigned: From CPU to BE
 
-#define cpu_to_be16(x)		(std::uint16_t)htobe16((std::uint16_t)(x))
-#define cpu_to_be32(x)		(std::uint32_t)htobe32((std::uint32_t)(x))
-#define cpu_to_be64(x)		(std::uint64_t)htobe64((std::uint64_t)(x))
+#define cpu_to_be16(x)		(std::uint16_t) htobe16 ((std::uint16_t)(x))
+#define cpu_to_be32(x)		(std::uint32_t) htobe32 ((std::uint32_t)(x))
+#define cpu_to_be64(x)		(std::uint64_t) htobe64 ((std::uint64_t)(x))
 
-#define cpu_to_be16p(x)		(std::uint16_t)htobe16(*(const std::uint16_t*)(x))
-#define cpu_to_be32p(x)		(std::uint32_t)htobe32(*(const std::uint32_t*)(x))
-#define cpu_to_be64p(x)		(std::uint64_t)htobe64(*(const std::uint64_t*)(x))
+#define cpu_to_be16p(x)		(std::uint16_t) htobe16 (*(const std::uint16_t*)(x))
+#define cpu_to_be32p(x)		(std::uint32_t) htobe32 (*(const std::uint32_t*)(x))
+#define cpu_to_be64p(x)		(std::uint64_t) htobe64 (*(const std::uint64_t*)(x))
 
 // Signed: From CPU to BE
 
-#define cpu_to_sbe16(x)		(std::int16_t)htobe16((std::int16_t)(x))
-#define cpu_to_sbe32(x)		(std::int32_t)htobe32((std::int32_t)(x))
-#define cpu_to_sbe64(x)		(std::int64_t)htobe64((std::int64_t)(x))
+#define cpu_to_sbe16(x)		(std::int16_t) htobe16 ((std::int16_t)(x))
+#define cpu_to_sbe32(x)		(std::int32_t) htobe32 ((std::int32_t)(x))
+#define cpu_to_sbe64(x)		(std::int64_t) htobe64 ((std::int64_t)(x))
 
-#define cpu_to_sbe16p(x)	(std::int16_t)htobe16(*(const std::int16_t*)(x))
-#define cpu_to_sbe32p(x)	(std::int32_t)htobe32(*(const std::int32_t*)(x))
-#define cpu_to_sbe64p(x)	(std::int64_t)htobe64(*(const std::int64_t*)(x))
+#define cpu_to_sbe16p(x)	(std::int16_t) htobe16 (*(const std::int16_t*)(x))
+#define cpu_to_sbe32p(x)	(std::int32_t) htobe32 (*(const std::int32_t*)(x))
+#define cpu_to_sbe64p(x)	(std::int64_t) htobe64 (*(const std::int64_t*)(x))
 
 #endif // _ENDIAN_H_
 
